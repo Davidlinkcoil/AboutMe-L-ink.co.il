e24a0a7fe31cdb1bde237a57c63f17eaf4cf304e
david <david@l-ink.co.il>
2024-02-10 20:14:30 +0200

    updated the websitet link with www

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mySocialLinks = {
     "linkedin":"https://www.linkedin.com/in/david-davarashvily-7538a51b8/",
     "tryhackme":"https://tryhackme.com/p/David.Divad",
     "microsoft learn" : "https://learn.microsoft.com/en-us/users/daviddavarashvily-9705/",
-    "website":"https://l-ink.co.il"
+    "website":"https://www.l-ink.co.il"
 }
 //Getting the buttons 
 const buttons = getElment('button');
